12bbab4832f101b5bb7c9e540646e4081bb87502
Surjendu kar <rahulgotp9988@gmail.com>
2024-08-29 13:26:45 +0530

    refactor: remove unused imports

--- frontend/src/components/CardGrid.tsx
+++ frontend/src/components/CardGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Box, Stack, styled, Typography } from "@mui/material";
 import Card from "./Card";
 import { useSearchParams } from "react-router-dom";
